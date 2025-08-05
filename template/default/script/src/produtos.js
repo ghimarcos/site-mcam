fcaa2a9c36648ec51e84df8a38f4648ccc40e4fc
Giovani Ribeiro <ghimarcos123@gmail.com>
2025-08-05 14:00:25 -0300

    Correções menus

---
+++ template/default/script/src/produtos.js
@@ -0,0 +1,314 @@
+// template/default/script/src/produtos.js
+const products = [
+  {
+    id: 1,
+    code: "5001",
+    name: "FLANGE CARRETA DIVERSAS",
+    category: "Flanges",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 2,
+    code: "5002",
+    name: "FLANGE CARRETA RANDON NOVA",
+    category: "Flanges",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 3,
+    code: "5003",
+    name: "FLANGE CARRETA MINI RANDON / MB 1113",
+    category: "Flanges",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 4,
+    code: "5045",
+    name: "FLANGE CARRETA RANDON NOVA SEM FURO C/ MARCAÇÃO 5F.",
+    category: "Flanges",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 5,
+    code: "5055",
+    name: "FLANGE CARRETA BONANO 160MM",
+    category: "Flanges",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 6,
+    code: "5006",
+    name: "FLANGE CARRETA DIVERSAS PESCOÇO CURTO",
+    category: "Flanges",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 7,
+    code: "5007",
+    name: "FLANGE CARRETA RANDON NOVA PESCOÇO CURTO",
+    category: "Flanges",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 8,
+    code: "5008",
+    name: "RÉGUA DIANTEIRA MB 915 / ACELLO / ATEGO",
+    category: "Metais 01",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 9,
+    code: "5009",
+    name: "RÉGUA DIANTEIRA VOLVO RODA RAIDADA",
+    category: "Metais 01",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 10,
+    code: "5010",
+    name: "RÉGUA DIANTEIRA RODA DISCO",
+    category: "Metais 01",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 11,
+    code: "5011",
+    name: "RÉGUA TRAÇÃO VOLVO REDUZIDA",
+    category: "Metais 01",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 12,
+    code: "5012",
+    name: "FLANGE RÉGUA VOLVO / FORD CANELA SECA",
+    category: "Flanges",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 13,
+    code: "3530",
+    name: "PLACA FECH. P/ PORTA CORRER",
+    category: "Metais 02",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 14,
+    code: "3532",
+    name: "PLACA FECH. P/ JANELA",
+    category: "Metais 02",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 15,
+    code: "3534",
+    name: "CONTRA FECH. P/ 3530",
+    category: "Metais 02",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 16,
+    code: "3536",
+    name: "CONTRA FECH. P/ 3532",
+    category: "Metais 02",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 17,
+    code: "3210 (1520X)",
+    name: "PLACA FECH. CENTRAL S/ PUXADOR",
+    category: "Metais 02",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 18,
+    code: "3211",
+    name: "CONTRA FECH. C/ ABA P/ 3210",
+    category: "Metais 02",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 19,
+    code: "3230",
+    name: "ESPELHO INJETADO REFORÇADO",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 20,
+    code: "3212",
+    name: "CONTRA FECH. S/ ABA P/ 3210",
+    category: "Metais 02",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 21,
+    code: "1520",
+    name: "PLACA INJETADA C/ PARAFUSOS EMBUTIDOS",
+    category: "Metais 02",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 22,
+    code: "1504-A",
+    name: "ESPELHO EM ALUM. COM BATENTE P/1520 INJETADO REFORÇADO",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 23,
+    code: "1531",
+    name: "CONTRA FECH. COM BATENTE P/1520",
+    category: "Metais 02",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 24,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 25,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 26,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 27,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 28,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 29,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 30,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 31,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 32,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 33,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 34,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 35,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 36,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 37,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 38,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+
+  {
+    id: 39,
+    code: "1101",
+    name: "DOBRADIÇA PIVOTANTE SUPERIOR REFORÇADA",
+    category: "Ferragens",
+    image: "assets/batedeira-1.png",
+  },
+];
